3741dd0f8d94213a0ed0d99b1587ea731872ce72
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2020-12-31 19:05:25 -0800

    Completed polybius tests, beginning substitution

--- src/substitution.js
+++ src/substitution.js
@@ -1,3 +1,21 @@
-function substitution(input, alphabet, encode = true) {}
+///// NO MATH IS REQUIRED FOR THIS ONE! :) /////
+
+function substitution(input, alphabet, encode = true) {
+  // convert the input to lowercase and to an array by letter
+  inputArray = input.toLowerCase().split("");
+
+  //
+  const result = inputArray.reduce((acc, char) => {
+    // ignore whitespace
+    if (char === " ") return acc + " ";
+    // convert to unicode, subtract 97
+    // this is the position of the result character from alphabet
+    const charCode = char.charCodeAt() - 97;
+    return acc + alphabet[charCode].toLowerCase();
+  }, "");
+
+  // return!
+  return result;
+}
 
 module.exports = substitution;
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,26 @@
-// Write your tests here!
+const substitution = require("../src/substitution");
+const expect = require("chai").expect;
+
+//Test sentence to use in most tests encoded/decoded
+const input = "The quick brown fox jumps over the lazy dog";
+const encoded = "may jworq ekgcf ugv pwdhl gxyk may sznb tgi";
+const alphabet = "ZERTYUIAOPQSDFGHJKLMWXCVBN";
+
+// Tests go here!
+describe("substitution", () => {
+  it("when not given an encode value, assumes encode is true", () => {
+    const result = substitution(input, alphabet);
+    const expected = substitution(input, alphabet, true);
+    expect(result).to.eql(expected);
+  });
+  it("when encode is true, returns a message correctly encoded", () => {
+    const result = substitution(input, alphabet);
+    expect(result).to.eql(encoded);
+  });
+  //it("when encode is false, returns a message correctly decoded", () => {})
+  //it("returns false if alphabet parameter is not a 26-char string", () => {})
+  //it("returns false if chars in alphabet parameter are not unique")
+  //it("returns the same result when input is lowercase or uppercase", () => {})
+  //it("returns the same result when alphabet is lowercase or uppercase", () => {})
+  //it("catches an error if input is not provided", () => {})
+});
